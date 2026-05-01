c540e2b2e7a9cacd7f7bbaadfd0f68c7f5a94b11
Laiba Aqeel <laibaaqeel@gmail.com>
2026-05-01 09:20:35 +0500

    fix axios baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: baseURL: 'https://resqplus-backend-production.up.railway.app/api',
+  baseURL: 'https://resqplus-backend-production.up.railway.app/api',
   timeout: 10000,
 });
 
